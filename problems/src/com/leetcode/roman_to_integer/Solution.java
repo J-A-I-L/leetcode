f94f7b33eb5a5e69002593413ec1e4d7e36375f7
Jose A. I. López <17472377+J-A-I-L@users.noreply.github.com>
2022-03-13 16:03:41 +0100

    13. Roman to Integer Easy https://leetcode.com/problems/roman-to-integer/

---
+++ problems/src/com/leetcode/roman_to_integer/Solution.java
@@ -0,0 +1,36 @@
+package com.leetcode.roman_to_integer;
+
+class Solution {
+    public int romanToInt(String s) {
+        int result = 0;
+
+        int lastRomanDigitValue = 0;
+
+        int length = s.length();
+        for (int i = length - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            int currentRomanDigitValue = translate(c);
+            if (lastRomanDigitValue > currentRomanDigitValue) {
+                result -= currentRomanDigitValue;
+            } else {
+                result += currentRomanDigitValue;
+            }
+            lastRomanDigitValue = currentRomanDigitValue;
+        }
+
+        return result;
+    }
+
+    private int translate(char c) {
+        return switch (c) {
+            case 'I' -> 1;
+            case 'V' -> 5;
+            case 'X'->  10;
+            case 'L'->  50;
+            case 'C'->  100;
+            case 'D'->  500;
+            case 'M'->  1000;
+            default -> throw new IllegalStateException("Unexpected value: " + c);
+        };
+    }
+}
