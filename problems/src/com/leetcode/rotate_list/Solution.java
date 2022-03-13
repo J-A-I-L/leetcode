1739df53571b32f9698830c1c8bfcd2b5ecb3164
Jose A. I. López <17472377+J-A-I-L@users.noreply.github.com>
2022-03-13 15:52:17 +0100

    61. Rotate List Medium https://leetcode.com/problems/rotate-list/

---
+++ problems/src/com/leetcode/rotate_list/Solution.java
@@ -0,0 +1,65 @@
+package com.leetcode.rotate_list;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || k == 0) {
+            return head;
+        }
+
+        ListNode originalHeadNode = head;
+
+        ListNode originalTail = reverse(head); // Note: after reversing the new head is the former tail, and vice-versa.
+        originalHeadNode.next = originalTail; // Make cyclic the current list.
+
+        ListNode finalHead = advanceKTimes(originalHeadNode, k);
+
+        ListNode finalTail = finalHead.next; // Make it acyclic again
+        finalHead.next = null;
+
+        /* finalHead = */ reverse(finalTail);
+        return finalHead;
+    }
+
+    /**
+     * Reverses the list. It mutates the list to reverse the order of its nodes.
+     * @param head of the ListNode to be reversed.
+     * @return The new head of the list, which was formerly the tail.
+     */
+    private ListNode reverse(ListNode head) {
+        ListNode previousNode = null;
+        ListNode currentNode = head;
+        while (currentNode != null) {
+            ListNode nextNode = currentNode.next;
+            currentNode.next = previousNode;
+            previousNode = currentNode;
+            currentNode = nextNode;
+        }
+        return previousNode;
+    }
+
+    /**
+     * Advances k times and returns the node of the new position.
+     * Loses any reference to the head.
+     * @param head The initial node  of the list to start from.
+     * @param k The number of times to advance.
+     * @return The node of the list after advancing k nodes.
+     */
+    private ListNode advanceKTimes(ListNode head, int k) {
+        ListNode currentNode = head;
+        while (k > 0) {
+            currentNode = currentNode.next;
+            k--;
+        }
+        return currentNode;
+    }
+}
---
+++ problems/src/com/leetcode/rotate_list/Test.java
@@ -0,0 +1,49 @@
+package com.leetcode.rotate_list;
+
+import java.util.*;
+
+public enum Test {
+    EXAMPLE_1(new int[]{1, 2, 3, 4, 5}, 2, new int[]{4, 5, 1, 2, 3}),
+    EXAMPLE_2(new int[]{0, 1, 2}, 4, new int[]{2, 0, 1}),
+    EXAMPLE_3(new int[]{}, 0, new int[]{}),
+    EXAMPLE_4(new int[]{}, 100, new int[]{}),
+    EXAMPLE_5(new int[]{1, 2, 3, 4, 5}, 0, new int[]{1, 2, 3, 4, 5}),
+    EXAMPLE_6(new int[]{1, 2}, 1, new int[]{2, 1}),
+    ;
+
+    public final ListNode head;
+    public final int k;
+
+    private final ListNode expectedResult;
+
+    Test(int[] elementsAsIntArray, int k, int[] expectedResultAsIntArray) {
+        this.head = ListNodeUtils.buildListNode(elementsAsIntArray);
+        this.k = k;
+        this.expectedResult = ListNodeUtils.buildListNode(expectedResultAsIntArray);
+    }
+
+    public void print(ListNode realResult) {
+        String expectedResultString = ListNodeUtils.listNodeToString(expectedResult);
+        String realResulString = ListNodeUtils.listNodeToString(realResult);
+
+        String formattedString = name() + ":" +
+                "\nexpected=" + expectedResultString +
+                ",\tgot=" + realResulString +
+                '\n' + (expectedResultString.equals(realResulString) ? "OK" : "KO") +
+                '\n';
+
+        System.out.println(formattedString);
+    }
+
+
+    public static void main(String[] args) {
+        final Solution solution = new Solution();
+        Arrays.stream(Test.values())
+                .forEach(testData -> {
+//                    System.out.println(ListNodeUtils.listNodeToString(testData.head));
+//                    System.out.println(ListNodeUtils.listNodeToString(solution.reverse(testData.head)));
+                    ListNode realResultListNode = solution.rotateRight(testData.head, testData.k);
+                    testData.print(realResultListNode);
+                });
+    }
+}
